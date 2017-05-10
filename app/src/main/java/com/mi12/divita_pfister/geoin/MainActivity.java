3f1c4484ec1e7a402793adafe060afa6b9ee3f38
MorganPfister <morgan.pfister@etu.utc.fr>
2017-05-10 19:06:24 +0200

    saving accelerometer values in a file

--- app/src/main/java/com/mi12/divita_pfister/geoin/MainActivity.java
+++ app/src/main/java/com/mi12/divita_pfister/geoin/MainActivity.java
@@ -9,31 +9,40 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorManager;
 import android.content.Context;
+import android.os.Environment;
 import android.os.SystemClock;
 import android.provider.Settings;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.view.View;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends Activity implements SensorEventListener, View.OnClickListener {
 
+    private int SENSOR_DELAY = 1000;
+
     private TextView labelX;
     private TextView labelY;
     private TextView labelZ;
-    private TextView WCET_result;
+    private TextView info;
 
-    private Button start_wcet;
+    private Button acquisition;
 
-    private boolean start_wcet_clicked = false;
-
-    private long start_time, end_time, result_time[];
-    private int cmp;
+    private boolean acquisition_started = false;
 
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
+
+    private List<Accelerometer> AccelerometerValue_l;
+    private Accelerometer[] AccelerometerValue_a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +52,10 @@ public class MainActivity extends Activity implements SensorEventListener, View.
         labelX = (TextView) findViewById(R.id.labelX);
         labelY = (TextView) findViewById(R.id.labelY);
         labelZ = (TextView) findViewById(R.id.labelZ);
-        WCET_result = (TextView) findViewById(R.id.WCET_result);
+        info = (TextView) findViewById(R.id.info);
 
-        start_wcet = (Button) findViewById(R.id.Start_wcet);
-        start_wcet.setOnClickListener(MainActivity.this);
+        acquisition = (Button) findViewById(R.id.acquisition);
+        acquisition.setOnClickListener(MainActivity.this);
 
         labelX.setText("X value");
         labelY.setText("Y value");
@@ -54,48 +63,33 @@ public class MainActivity extends Activity implements SensorEventListener, View.
 
         senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+        senSensorManager.registerListener(this, senAccelerometer, SENSOR_DELAY);
 
-        cmp = 0;
-        result_time = new long[10000];
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        int max_loop = 100;
-        end_time = System.nanoTime();
-        if (start_wcet_clicked){
-            result_time[cmp] = end_time - start_time;
-            if (cmp < max_loop) cmp++;
-            else {
-                long min = result_time[0], max = result_time[0];
-                long mean = 0;
-                for (int i=0;i<max_loop;i++){
-                    if (min > result_time[i]) min = result_time[i];
-                    else if (max < result_time[i]) max = result_time[i];
-                    mean += result_time[i];
-                }
-                mean /= max_loop;
-                WCET_result.setText("Min: " + Long.toString(min) + " ns\nMax : " + Long.toString(max) + " ns\nMean: " + Long.toString(mean) + " ns" );
-            }
-        }
         Sensor mySensor = sensorEvent.sensor;
 
-        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER && acquisition_started == true) {
             float x = sensorEvent.values[0];
             float y = sensorEvent.values[1];
             float z = sensorEvent.values[2];
 
+            Accelerometer AccelerometerValue = new Accelerometer(x, y, z);
+
             labelX.setText(Float.toString(x));
             labelY.setText(Float.toString(y));
             labelZ.setText(Float.toString(z));
+
+            AccelerometerValue_l.add(AccelerometerValue);
         }
-        start_time = System.nanoTime();
     }
 
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
 
     protected void onPause() {
         super.onPause();
@@ -104,10 +98,33 @@ public class MainActivity extends Activity implements SensorEventListener, View.
 
     protected void onResume() {
         super.onResume();
-        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        senSensorManager.registerListener(this, senAccelerometer, SENSOR_DELAY);
     }
 
-    public void onClick(View v) {
-        start_wcet_clicked = true;
+    public void onClick(View view) {
+        if (view.equals(acquisition)) {
+            if (acquisition_started == false) {
+                AccelerometerValue_l = new ArrayList<Accelerometer>();
+                acquisition_started = true;
+                acquisition.setText("Arrêter l'acquisition");
+            } else {
+                acquisition_started = false;
+                AccelerometerValue_a = AccelerometerValue_l.toArray(new Accelerometer[AccelerometerValue_l.size()]);
+                acquisition.setText("Commencer l'acquisition");
+
+                File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                File file = new File(path, "test.txt");
+                try {
+                    path.mkdirs();
+                    OutputStream os = new FileOutputStream(file);
+                    for (int i = 0; i < AccelerometerValue_a.length; i++) {
+                        os.write((AccelerometerValue_a[i].toString() + "\n").getBytes());
+                    }
+                    os.close();
+                } catch (IOException e) {
+                    Log.w("External Storage", "Error writing" + file, e);
+                }
+            }
+        }
     }
 }
